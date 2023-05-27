f1ecd868915b1ac6e26538baf95ad27650079450
nicoledbianchin <nicoledbianchin@gmail.com>
2023-05-27 17:24:27 -0300

    arruma e cria mais conteudos na sessao saiba mais

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var dropdown = document.querySelector(".dropdown");
-    var dropdownContent = document.querySelector(".dropdown-content");
-  
-    dropdown.addEventListener("click", function() {
-      dropdown.classList.toggle("open");
+    var dropdownButtons = document.querySelectorAll(".dropbtn");
+    
+    dropdownButtons.forEach(function(button) {
+      button.addEventListener("click", function() {
+        var content = this.nextElementSibling;
+        
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
     });
   });
   
